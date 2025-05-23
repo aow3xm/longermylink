616f117a977ce3f022f284ed71c7d1f26ce15a37
aow3xm <phamminh.aow3@gmail.com>
2025-05-24 04:29:28 +0700

    initial project

---
+++ config/page.ts
@@ -0,0 +1,38 @@
+import localFont from 'next/font/local';
+
+export const paths = {
+  home: '/',
+  profile: '/profile',
+  auth: {
+    login: '/login',
+    register: '/regsiter',
+    forgotPassword: '/forgot-password',
+    resetPassword: '/reset-password/:token',
+  },
+};
+
+export const geist = localFont({
+  src: [
+    {
+      path: './fonts/Geist-Regular.ttf',
+      weight: '400',
+    },
+    {
+      path: './fonts/Geist-Bold.ttf',
+      weight: '700',
+    },
+  ],
+});
+
+export const beVietNam = localFont({
+  src: [
+    {
+      path: './fonts/BeVietnamPro-Bold.ttf',
+      weight: '700',
+    },
+    {
+      path: './fonts/BeVietnamPro-Regular.ttf',
+      weight: '400',
+    },
+  ],
+});
