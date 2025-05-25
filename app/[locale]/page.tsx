dc98b1c0bf567f4da90ca125b32aa50b354c9989
aow3xm <phamminh.aow3@gmail.com>
2025-05-25 23:15:35 +0700

    fix: remove unused import

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,6 @@
 import { AvatarSelect } from '@/components/AvatarSelect';
 import { Hero } from '@/components/Hero';
-import { useTranslations } from 'next-intl';
 export default function HomePage() {
-  const t = useTranslations('HomePage');
   return (
     <div>
       <AvatarSelect/>
