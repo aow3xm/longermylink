97e5f23244b6a8e051ce0c88e0d7e375f3e2aa00
aow3xm <phamminh.aow3@gmail.com>
2025-06-01 22:48:36 +0700

    feat: implement delete link schema

--- lib/schema/personal/index.ts
+++ lib/schema/personal/index.ts
@@ -1,13 +1,17 @@
-import { InferInput, url, nonEmpty, object, pipe, string, trim } from 'valibot';
+import { InferInput, nonEmpty, number, object, pipe, string, trim } from 'valibot';
 
 export const updateAvatarSchema = object({
     link: pipe(string(), trim())
 })
 
 export const generateLinkSchema = object({
-    original: pipe(string(), url(), trim(), nonEmpty()),
-    path: pipe(string(), trim()),
+    original: pipe(string(), trim(), nonEmpty()),
+})
+
+export const deleteLinkSchema = object({
+    id: pipe(number())
 })
 export type GenerateLinkData = InferInput<typeof generateLinkSchema>
+export type DeleteLinkData = InferInput<typeof deleteLinkSchema>
 
 export type UpdateAvatarData = InferInput<typeof updateAvatarSchema>
