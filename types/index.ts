86311585bca371880b65235f9c06bd1eaedb1d59
aow3xm <phamminh.aow3@gmail.com>
2025-06-02 15:27:54 +0700

    refactor: move types into @/types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,17 @@
+import { routing } from '@/i18n/routing';
+import { authClient } from '@/lib/auth/client';
 import { link } from '@/lib/db/schema';
 
 export type Link = typeof link.$inferSelect
+
+export type AuthErrorCode = keyof typeof authClient.$ERROR_CODES
+
+export type Locale = typeof routing.locales[number]
+
+export type GetLinksResponse = {
+  error?: string;
+  data?: {
+    links: Link[];
+    next: number | null;
+  };
+};
